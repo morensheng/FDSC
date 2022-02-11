b0933fd35e7e0c5b0f8a44397814418507d2eedb
morensheng <3104810349@qq.com>
2022-02-11 20:22:26 +0800

    over

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
-// // var LCalender = require('MobileCalendar');
-// import LCalender from './LCalendar.js';
-// // 初始化日期插件
-// var calendar = new LCalendar();
-// calendar.init({
-//     'trigger': '#demo1',//标签id
-//     'type': 'date',//date 调出日期选择 datetime 调出日期时间选择 time 调出时间选择 ym 调出年月选择
-//     'minDate': '1900-1-1',//最小日期 注意：该值会覆盖标签内定义的日期范围
-//     'maxDate': '2016-3-18'//最大日期 注意：该值会覆盖标签内定义的日期范围
-// });
+// input的非空验证
+function isEmpty() {
+    const iptval = document.getElementsByName('myipt');
+    const redtxt = document.getElementsByClassName('redtxt');
+    for (let i = 0; i < iptval.length; i++) {
+        redtxt[i].classList.add('hid');
+        if (iptval[i].value === '') {
+            redtxt[i].classList.remove('hid');
+        }
+        else {
+            console.log('已提交表单');
+        }
+
+
+    }
+
+}
